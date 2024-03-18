f8ca8958231ba10ff57819764e6dd415a9cae2d4
Timur <timur.z@wecandevelopit.com>
2024-03-18 11:09:59 +0300

    update endpoint

--- Lib/PlisioGateway.php
+++ Lib/PlisioGateway.php
@@ -5,7 +5,7 @@ namespace Plisio\PlisioGateway\Lib;
 class PlisioGateway
 {
     protected $secretKey = '';
-    public $apiEndPoint = 'https://plisio.net/api/v1';
+    public $apiEndPoint = 'https://api.plisio.net/api/v1';
 
     public function __construct($secretKey)
     {
